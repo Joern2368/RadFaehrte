9a5450dda466fbb5e3685dbd8cf01581b7826d81
Jörn Frankenfeld <frankenfeld@icloud.com>
2026-07-17 14:08:04 +0200

    Initial Commit

---
+++ FahrradApp/FahrradAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FahrradAppApp.swift
+//  FahrradApp
+//
+//  Created by Jörn Frankenfeld on 17.07.26.
+//
+
+import SwiftUI
+
+@main
+struct FahrradAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
